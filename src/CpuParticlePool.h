d4d69a7ad076cc8eaca7b189ff17d94301ed7640
Pantareje <62074882+Pantareje@users.noreply.github.com>
2024-11-24 23:33:24 +0100

    Añadidos comentarios sobre el tipo de datos.

--- src/CpuParticlePool.h
+++ src/CpuParticlePool.h
@@ -3,6 +3,14 @@
 #include <cassert>
 #include <vector>
 
+/*
+ * En este ejemplo, las partículas no son destruidas sino reiniciadas a un nuevo estado
+ * inicial. Si la simulación de partículas requiriera crear y destruir partículas de
+ * manera arbitraria durante la simulación se debería usar otra estructura, como por
+ * ejemplo una lista libre, la cual permite crear y destruir objetos en un vector de
+ * manera arbitraria con tiempo de ejecución O(1).
+ */
+
 template<typename T, typename U>
 class CpuParticlePool {
     size_t m_maxParticles;
@@ -20,16 +28,9 @@ public:
     [[nodiscard]] size_t GetMaxCount() noexcept { return m_maxParticles; }
     [[nodiscard]] size_t GetCount() const noexcept { return m_particleCount; }
 
-
     [[nodiscard]] T& GetParticle(size_t i) noexcept { return m_particles[i]; }
-    [[nodiscard]] const T& GetParticle(size_t i) const noexcept { return m_particles[i]; }
-
     [[nodiscard]] U& GetOfflineData(size_t i) noexcept { return m_offlineData[i]; }
-    [[nodiscard]] const U& GetOfflineData(size_t i) const noexcept { return m_offlineData[i]; }
-
-
     [[nodiscard]] const T* GetParticlesBuffer() const noexcept { return m_particles.data(); }
-
 
     void AddParticle(const T& particle, const U& offlineData) {
         if (m_particleCount == m_maxParticles) return;
